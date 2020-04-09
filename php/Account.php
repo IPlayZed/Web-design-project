00c267472e252d80f217bf199ccf2adb39c06a54
IPlayZed <pauljouser@gmail.com>
2020-04-09 20:55:11 +0200

    ide került

---
+++ php/Account.php
@@ -0,0 +1,50 @@
+<?php
+    //A regisztrációnál megadott adatokat tárolja el.
+    class Account {
+        private $email;
+        private $username;
+        private $password;
+        private $born;
+        private $gender;
+
+        public function setEmail($email) {
+            $this->email = $email;
+        }
+        public function setUsername($username) {
+            $this->username = $username;
+        }
+        public function setPassword($password) {
+            $this->password = $password;
+        }
+        public function setBorn($born) {
+            $this->born = $born;
+        }
+        public function setGender($gender) {
+            $this->gender = $gender;
+        }
+
+        public function getEmail() {
+            return $this->email;
+        }
+        public function getUsername() {
+            return $this->username;
+        }
+        public function getPassword() {
+            return $this->password;
+        }
+        public function getBorn() {
+            return $this->born;
+        }
+        public function getGender() {
+            return $this->gender;
+        }
+
+        public function __construct($email, $username, $password, $born, $gender) {
+            $this->setEmail($email);
+            $this->setUsername($username);
+            $this->setPassword($password);
+            $this->setBorn($born);
+            $this->setGender($gender);
+        }
+    } 
+?>
